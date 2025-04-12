b79951ad8e5bd30952e7f5dea93fc70ac288c20e
MahAzevedo <madavetto@gmail.com>
2025-04-12 18:03:06 -0300

    init commit

---
+++ app.js
@@ -0,0 +1,87 @@
+// alert('Funcionou!!')
+
+// let numeroSecreto = 29;
+
+
+
+// comparar é tipo se o cara é igual então usa === , condição --> if  , if else
+
+// if(numeroSecreto === 10) {
+    // 'Vc acertou'
+// } else {
+    // 'Vc errou'
+// }
+
+// const blablabla = 4;
+// if(blablabla > 4) {};
+
+
+// let numeroSecreto = 3;
+
+// let caixaQueRecebeInformacaoFicaAquiDesseLado = prompt('Escolha um número entre 1 e 10')
+
+// if ( caixaQueRecebeInformacaoFicaAquiDesseLado == numeroSecreto
+// ) {
+//     console.log('Vc acertou Mádjan! Vc domina lógica de programação! Parabéns por se reciclar!')
+// }
+
+
+// let madjan = 1;
+// let sabeTudo = prompt('Escolha um número entre 1 e 10')
+
+// if(madjan == sabeTudo) {
+//     console.log('Mádjan precisa praticar mais, porém, domina toda a arte do saber!')
+// }
+
+// let mensagemDeBoasVindas = 'Boas vindas ao App';
+// alert('mensagemDeBoasVindas')
+
+
+// let senhaDoSistema = "senhaTeste!";
+
+// let senha = prompt("Digite a senha do sistema:");
+
+// if (senha == senhaDoSistema){
+//     alert("Acesso ao sistema garantido");
+// }
+
+// alert('Boas vindas ao nosso site!')
+
+// let nome = 'Lua';
+
+// let idade = 25;
+
+// let numeroDeVendas  = 50;
+
+// let saldoDisponivel = 1000;
+
+// alert('Erro! Preencha todos os campos')
+
+// //
+// let mensagemDeErro = "Erro! Preencha todos os campos";
+// alert(mensagemDeErro)
+
+// //
+// let nomee = prompt('Nome de usuário')
+
+// let idadee = prompt('idade')
+// if(idadee >= 18) {
+//     console.log('Pode tirar a habilitação!')
+// }
+
+
+
+let chute = 2;
+let numeroSecretoo = 2;
+
+if(chute == numeroSecretoo) {
+    console.log('Vc acertou mesmo!')
+} else {    
+    alert('Vc acertou, conseguiu de novo!!')
+}
+
+
+
+
+
+
